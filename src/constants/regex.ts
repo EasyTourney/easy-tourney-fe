5a8c11c76fb10a1d9d734055af970b4521f5435e
Việt Thế Mai <viet.the.mai@mgm-tp.com>
2023-12-04 10:32:20 +0000

    MGMIDN-942 : Create Category
    Merge in DA-NANG-INTERNSHIP/easy-tourney-fe from feature/MGMIDN-942-sp-1-create-category-fe-2 to master
    
    Squashed commit of the following:
    
    commit 26cdc6587b13a818d9deac6ed0c3523f515dc399
    Author: Viet <maitheviet14@gmail.com>
    Date:   Mon Dec 4 17:25:51 2023 +0700
    
        MGMIDN-942 : Create Category-FE
    
    commit 76659b34dbabba04bf13547343abb2feae022046
    Author: Viet <maitheviet14@gmail.com>
    Date:   Mon Dec 4 16:28:30 2023 +0700
    
        MGMIDN-942 : Create Category
    
    commit 11508c05f69defe19550f3d06f024113e658d183
    Author: Viet <maitheviet14@gmail.com>
    Date:   Mon Dec 4 16:26:57 2023 +0700
    
        MGMIDN-942 : Create Category-FE
    
    commit eb099dfcc297db8f261f28245c8733ab7346342b
    Author: Viet <maitheviet14@gmail.com>
    Date:   Mon Dec 4 13:04:12 2023 +0700
    
        MGMIDN-942 : Create Category

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -1,5 +1,5 @@
 export const EMAIL_REGEX =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-export const CHARACTERS_REGEX = /^[^\s!@#$%^&*(),.?":{}|<>]+(\s[^\s!@#$%^&*(),.?":{}|<>]+[^\s!@#$%^&*(),.?":{}|<>])?$/
-export const SPACE_START_REGEX = /^\s/
-export const SPACE_END_REGEX = /\s$/
+export const CHARACTERS_REGEX = /^[\p{L}0-9]+(?:\s[\p{L}0-9]+)*$/u
+export const SPACE_START_REGEX = /^\s/u
+export const SPACE_END_REGEX = /\s$/u
